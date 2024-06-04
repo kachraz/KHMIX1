c55fde3c74ef96936323ae7c7ca67f26f7066bb4
kachraz <110726496+kachraz@users.noreply.github.com>
2024-06-04 01:24:26 +0000

    .

--- kh/kch.fish
+++ kh/kch.fish
@@ -1,9 +1,9 @@
 #!/bin/fish
-/workspaces/KHMIX1/pnp.fish
-/workspaces/KHMIX1/fixrs.fish
-/workspaces/KHMIX1/st.sh
-/workspaces/KHMIX1/aryt.fish
-/workspaces/KHMIX1/tgpt.fish
+/workspaces/KHMIX1/kh/pnp.fish
+/workspaces/KHMIX1/kh/fixrs.fish
+/workspaces/KHMIX1/kh/st.sh
+/workspaces/KHMIX1/kh/aryt.fish
+/workspaces/KHMIX1/kh/tgpt.fish
 /workspaces/KHMIX1/bun.fish
 /workspaces/KHMIX1/tmuxx.fish
 
