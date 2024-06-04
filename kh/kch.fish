29163494399ade7dc19885f5ccaae37a597c48a8
kachraz <110726496+kachraz@users.noreply.github.com>
2024-06-04 01:19:25 +0000

    .

---
+++ kh/kch.fish
@@ -0,0 +1,10 @@
+#!/bin/fish
+/workspaces/h1/mx/pnp.fish
+/workspaces/h1/mx/fixrs.fish
+/workspaces/h1/mx/st.sh
+/workspaces/h1/mx/aryt.fish
+/workspaces/h1/mx/tgpt.fish
+/workspaces/h1/mx/bun.fish
+/workspaces/h1/mx/tmuxx.fish
+
+
